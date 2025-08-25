b853ef82113e65e963df032b4bfc36dd5caa3108
abelucsd <65742650+abelucsd@users.noreply.github.com>
2025-08-25 11:25:51 -0400

    choire: add category and product routers to app.ts.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,8 @@ import * as swaggerUi from 'swagger-ui-express';
 import { swaggerDocument } from './config/swagger.js';
 import { userRouter } from './users/routes.js';
 import { customerRouter } from './customers/routes.js';
+import { categoryRouter } from './categories/routes.js';
+import { productRouter } from './products/routes.js';
 
 const app = express();
 app.use(express.json());
@@ -13,5 +15,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // routes
 app.use('/users', userRouter);
 app.use('/customers', customerRouter);
+app.use('/categories', categoryRouter);
+app.use('/products', productRouter);
 
 export default app;
